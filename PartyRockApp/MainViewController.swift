595073a2a1ad4cd9630cc90437ba79ff0660bf66
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-16 11:14:26 -0300

    :construction: Adds views

---
+++ PartyRockApp/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Munir Wanis on 16/04/17.
+//  Copyright © 2017 Munir Wanis. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
